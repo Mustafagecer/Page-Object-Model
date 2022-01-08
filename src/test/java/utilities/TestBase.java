6489cad7b640ece976a6b08f45b1413bbafa89ea
Mustafa Gecer <h.musti.27@gmail.com>
2022-01-08 22:13:51 +0100

    first commit

---
+++ src/test/java/utilities/TestBase.java
@@ -0,0 +1,38 @@
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+public abstract class TestBase {
+
+    // abstract yaparak bu class'dan obje olusturulmasinin onune geceriz
+
+    protected static WebDriver driver;
+
+    // biz test base class'i sadece extends ile inherit ederek kullanacagiz
+    // dolayisiyla olusturdugumuz driver variable'i icin protected access modifier'i seciyoruz
+
+    @BeforeClass
+    public void setup(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+
+
+    }
+    @AfterClass
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(3000);
+        //driver.quit();
+    }
+}
